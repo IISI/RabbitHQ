01131a60ed24da1c7a5398d6be47d07327ee45bc
Chih-Liang Chang <itscchang@gmail.com>
2011-02-16 09:57:11 +0800

    增加 p2 自動更新的程式碼。

--- tw.com.iisi.rabbithq/src/tw/com/iisi/rabbithq/ApplicationWorkbenchAdvisor.java
+++ tw.com.iisi.rabbithq/src/tw/com/iisi/rabbithq/ApplicationWorkbenchAdvisor.java
@@ -3,6 +3,7 @@ package tw.com.iisi.rabbithq;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipselabs.p2.rcpupdate.utils.P2Util;
 
 /**
  * This workbench advisor creates the window advisor, and specifies the
@@ -17,6 +18,11 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
     public String getInitialWindowPerspectiveId() {
         return Perspective.ID;
+    }
+
+    @Override
+    public void preStartup() {
+        P2Util.checkForUpdates();
     }
 
 }
